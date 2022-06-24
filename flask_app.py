9506fc8db6c242c0c38e972d9dced3c7e45a0a37
noahzhang <noahzhang123@gmail.com>
2022-06-24 17:29:43 +1000

    template

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,27 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
+import pandas as pd
+import numpy as np
+import ghhops_server as hs
 from flask import Flask
 
 app = Flask(__name__)
+hops = hs.Hops(app)
 
-@app.route('/')
-def hello():
-    return 'linkstart!'
+@hops.component( 
+    "/pointat", 
+    name="PointAt",    
+    description="Get point along curve",    
+    icon="examples/pointat.png",    
+    inputs=[        
+        hs.HopsCurve("Curve", "C", "Curve to evaluate"),       
+        hs.HopsNumber("t", "t", "Parameter on Curve to evaluate"),
+        ],    
+    outputs=[        
+        hs.HopsPoint("P", "P", "Point on curve at t")  
+        ]
+    )
+
+def pointat(curve, t):
+    return curve.PointAt(t)
+
+if __name__ == "__main__":
+    app.run()
